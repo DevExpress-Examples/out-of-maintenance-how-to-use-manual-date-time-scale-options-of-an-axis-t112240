0c5863f229dea8de9c3ecd5263ff1902aec80072
DevExpressExampleBot <fakeEmail>
2022-12-18 21:10:43 +0400

    Source auto update [skip ci]

--- VB/T112240_CS/WpfApplication19/MainWindow.xaml.vb
+++ VB/T112240_CS/WpfApplication19/MainWindow.xaml.vb
@@ -1,4 +1,4 @@
-﻿Imports System
+Imports System
 Imports System.Collections.Generic
 Imports System.Globalization
 Imports System.Windows
@@ -7,79 +7,76 @@ Imports System.Xml.Linq
 
 Namespace WpfApplication19
 
-	Partial Public Class MainWindow
-		Inherits Window
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-			DataContext = New MyModel()
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+            DataContext = New MyModel()
+        End Sub
+    End Class
 
-	Public Class DateTimePoint
-'INSTANT VB NOTE: The field value was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private value_Conflict As Double
-'INSTANT VB NOTE: The field argument was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private argument_Conflict As DateTime
+    Public Class DateTimePoint
 
-		Public ReadOnly Property Value() As Double
-			Get
-				Return value_Conflict
-			End Get
-		End Property
+        Private valueField As Double
 
-		Public ReadOnly Property Argument() As DateTime
-			Get
-				Return argument_Conflict
-			End Get
-		End Property
+        Private argumentField As Date
 
-		Public Sub New(ByVal argument As DateTime, ByVal value As Double)
-			Me.argument_Conflict = argument
-			Me.value_Conflict = value
-		End Sub
-	End Class
+        Public ReadOnly Property Value As Double
+            Get
+                Return valueField
+            End Get
+        End Property
 
-	Public Class MyModel
-'INSTANT VB NOTE: The field rate was renamed since Visual Basic does not allow fields to have the same name as other class members:
-		Private rate_Conflict As New List(Of DateTimePoint)()
+        Public ReadOnly Property Argument As Date
+            Get
+                Return argumentField
+            End Get
+        End Property
 
-		Public ReadOnly Property Rate() As List(Of DateTimePoint)
-			Get
-				Return rate_Conflict
-			End Get
-		End Property
+        Public Sub New(ByVal argument As Date, ByVal value As Double)
+            argumentField = argument
+            valueField = value
+        End Sub
+    End Class
 
+    Public Class MyModel
 
-		Public Sub New()
+        Private rateField As List(Of DateTimePoint) = New List(Of DateTimePoint)()
 
-			LoadPoints(rate_Conflict, LoadFromFile("/GbpUsdRate.xml"))
-		End Sub
+        Public ReadOnly Property Rate As List(Of DateTimePoint)
+            Get
+                Return rateField
+            End Get
+        End Property
 
-		Private Function LoadFromFile(ByVal xmlFile As String) As XDocument
-			Return LoadXmlFromResources(xmlFile)
-		End Function
+        Public Sub New()
+            LoadPoints(rateField, LoadFromFile("/GbpUsdRate.xml"))
+        End Sub
 
-		Public Shared Function LoadXmlFromResources(ByVal fileName As String) As XDocument
+        Private Function LoadFromFile(ByVal xmlFile As String) As XDocument
+            Return LoadXmlFromResources(xmlFile)
+        End Function
 
-			Try
-				fileName = "/WpfApplication19;component" & fileName
-				Dim uri As New Uri(fileName, UriKind.RelativeOrAbsolute)
-				Dim info As StreamResourceInfo = Application.GetResourceStream(uri)
-				Return XDocument.Load(info.Stream)
-			Catch
-				Return Nothing
-			End Try
-		End Function
+        Public Shared Function LoadXmlFromResources(ByVal fileName As String) As XDocument
+            Try
+                fileName = "/WpfApplication19;component" & fileName
+                Dim uri As Uri = New Uri(fileName, UriKind.RelativeOrAbsolute)
+                Dim info As StreamResourceInfo = Application.GetResourceStream(uri)
+                Return XDocument.Load(info.Stream)
+            Catch
+                Return Nothing
+            End Try
+        End Function
 
-		Private Sub LoadPoints(ByVal rate As List(Of DateTimePoint), ByVal document As XDocument)
-			If rate IsNot Nothing AndAlso document IsNot Nothing Then
-				For Each element As XElement In document.Descendants("CurrencyRate")
-					Dim argument As DateTime = DateTime.Parse(element.Element("DateTime").Value)
-					Dim value As Double = Double.Parse(element.Element("Rate").Value, CultureInfo.InvariantCulture)
-					rate.Add(New DateTimePoint(argument, value))
-				Next element
-			End If
-		End Sub
-	End Class
+        Private Sub LoadPoints(ByVal rate As List(Of DateTimePoint), ByVal document As XDocument)
+            If rate IsNot Nothing AndAlso document IsNot Nothing Then
+                For Each element As XElement In document.Descendants("CurrencyRate")
+                    Dim argument As Date = Date.Parse(element.Element("DateTime").Value)
+                    Dim value As Double = Double.Parse(element.Element("Rate").Value, CultureInfo.InvariantCulture)
+                    rate.Add(New DateTimePoint(argument, value))
+                Next
+            End If
+        End Sub
+    End Class
 End Namespace
